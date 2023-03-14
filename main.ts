be0d8b6bc1b573a73e3e4ddd152606a78d819270
Wayne Hamilton <wth101@users.noreply.github.com>
2023-03-14 13:30:31 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,71 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -200
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 6))
+    info.changeLifeBy(-1)
+    info.startCountdown(12)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 6))
+    sprites.destroy(otherSprite)
+    scene.cameraShake(4, 200)
+    info.changeLifeBy(-1)
+    info.startCountdown(12)
+})
+let bogey: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundColor(9)
+tiles.setTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . 2 2 2 2 2 2 2 2 . . . . 
+    . . . 2 4 2 2 2 2 2 2 c 2 . . . 
+    . . 2 c 4 2 2 2 2 2 2 c c 2 . . 
+    . 2 c c 4 4 4 4 4 4 2 c c 4 2 d 
+    . 2 c 2 e e e e e e e b c 4 2 2 
+    . 2 2 e b b e b b b e e b 4 2 2 
+    . 2 e b b b e b b b b e 2 2 2 2 
+    . e e 2 2 2 e 2 2 2 2 2 e 2 2 2 
+    . e e e e e e f e e e f e 2 d d 
+    . e e e e e e f e e f e e e 2 d 
+    . e e e e e e f f f e e e e e e 
+    . e f f f f e e e e f f f e e e 
+    . . f f f f f e e f f f f f e . 
+    . . . f f f . . . . f f f f . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.ay = 500
+mySprite.setPosition(0, 105)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 6))
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite, 100, 0)
+info.setLife(3)
+info.startCountdown(12)
+game.onUpdateInterval(2000, function () {
+    if (Math.percentChance(50)) {
+        bogey = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 4 4 4 4 4 . . . . . . 
+            . . . 4 4 4 5 5 5 d 4 4 4 4 . . 
+            . . 4 d 5 d 5 5 5 d d d 4 4 . . 
+            . . 4 5 5 1 1 1 d d 5 5 5 4 . . 
+            . 4 5 5 5 1 1 1 5 1 1 5 5 4 4 . 
+            . 4 d d 1 1 5 5 5 1 1 5 5 d 4 . 
+            . 4 5 5 1 1 5 1 1 5 5 d d d 4 . 
+            . 2 5 5 5 d 1 1 1 5 1 1 5 5 2 . 
+            . 2 d 5 5 d 1 1 1 5 1 1 5 5 2 . 
+            . . 2 4 d d 5 5 5 5 d d 5 4 . . 
+            . . . 2 2 4 d 5 5 d d 4 4 . . . 
+            . . 2 2 2 2 2 4 4 4 2 2 2 . . . 
+            . . . 2 2 4 4 4 4 4 4 2 2 . . . 
+            . . . . . 2 2 2 2 2 2 . . . . . 
+            `, SpriteKind.Enemy)
+        bogey.setPosition(scene.cameraProperty(CameraProperty.X) + 80, 100)
+        bogey.setVelocity(-100, 0)
+        mySprite.setFlag(SpriteFlag.AutoDestroy, true)
+    }
+})
